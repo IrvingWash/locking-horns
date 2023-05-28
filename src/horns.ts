9fd83e2172093956b5793590c9f7f6d088e82c21
Washington Irving <iamwashingyou@gmail.com>
2023-05-28 21:12:34 +0300

    add herds

--- src/horns.ts
+++ src/horns.ts
@@ -1,24 +1,42 @@
 type HornName = string;
+type Herd = InstanceType<typeof BroadcastChannel>;
+type HerdAction<T> = () => T;
 
-export class Horns {
-	/**
-	 * Release the lock to make each tab independent
-	 */
-	public unlock?: () => void;
+export class Horns<T = {}> {
+	private _unlock?: () => void;
 
 	/**
 	 * Lock the tabs so only one of them is fetching data
-	 * and all the others are getting the data from the fetching one
+	 * and all the others are getting the data from the fetching one.
 	 */
-	public async lock(name: HornName, action: () => void): Promise<void> {
+	public async lock(name: HornName, herd: Herd, action: HerdAction<T>): Promise<void> {
 		const deferredAction = async(): Promise<void> => new Promise(
 			(resolve: () => void) => {
 				this.unlock = resolve;
 
-				action();
+				const data = action();
+
+				herd.postMessage(data);
 			}
 		);
 
 		await navigator.locks.request(name, deferredAction);
+	}
+
+	/**
+	 * Create a herd of tabs between which the data will be shared
+	 */
+	public createHerd(name: HornName): InstanceType<typeof BroadcastChannel> {
+		return new BroadcastChannel(name);
+	}
+
+	/**
+	 * Release the lock to make each tab independent.
+	 * This method is available only after locking
+	 */
+	public unlock(herd: Herd): void {
+		herd.close();
+
+		this._unlock?.();
 	}
 }
