73a1de20cba6ea15a9992dbc95429081b1fb078e
Washington Irving <iamwashingyou@gmail.com>
2023-05-28 20:27:48 +0300

    init project

---
+++ src/index.test.ts
@@ -0,0 +1,12 @@
+import {
+	describe,
+	it,
+	expect,
+} from '@jest/globals';
+
+describe('Dummy test', () => {
+	it('shout do dummy work', () => {
+		// Bonk
+		expect(true).toBe(true);
+	});
+});
